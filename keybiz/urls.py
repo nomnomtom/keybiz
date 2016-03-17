a15c6b102215699c9936ec617eeac90bfd27edff
Tom Priebe <tom@hobbitshop24.biz>
2016-03-17 19:35:21 +0100

    template bugs, +navbar, +logout

--- keybiz/urls.py
+++ keybiz/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^keycheck/', include('keycheck.urls')),
-	url(r'^accounts/login', 'django.contrib.auth.views.login', {'template_name': 'keycheck/login.html'}, name='login'),
+	url(r'^accounts/login', auth_views.login, {'template_name': 'keycheck/login.html'}, name='login'),
+	url(r'^accounts/logout', auth_views.logout, name='logout'),
 ]
