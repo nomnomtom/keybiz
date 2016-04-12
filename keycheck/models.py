91b38eec86ff86448b1f70963ed2b89a0d07e12b
Tom Priebe <tom@hobbitshop24.biz>
2016-04-12 12:16:33 +0200

    signing works on the console, need to put it somewhere though

--- keycheck/models.py
+++ keycheck/models.py
@@ -4,16 +4,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 from subprocess import Popen, PIPE
-from re import match
+from re import match, search
 from tempfile import NamedTemporaryFile
 
 
 class GpgKey(models.Model):
 	keydata = models.TextField(unique=True)
 
+	def __init__(self, *args, **kwargs):
+		super(GpgKey, self).__init__(*args, **kwargs)
+		# TODO: only import if key doesn't exist
+		tempkey = NamedTemporaryFile(delete=False)
+		try:
+			tempkey.write(self.keydata)
+			tempkey.close() # we need to close this so the data is written >.<
+			cmd = self.GPGCommand('--import', tempkey.name)
+			p = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE) # import key to keyring
+			out, err = p.communicate() # wait for p to finish
+		finally:
+			tempkey.close()
+
+
 	def getKeyID(self):
 		''' return long key id '''
-		p = Popen([settings.GPG_BIN, '--with-fingerprint', '--keyid-format', 'LONG'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+		cmd = self.GPGCommand('--with-fingerprint', '--keyid-format', 'LONG')
+		p = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
 		out, err = p.communicate(input=self.keydata)
 		if out != "":
 			out = out.split("\n")[0] #id is in first line
@@ -33,7 +48,8 @@ class GpgKey(models.Model):
 		keyid = self.getKeyID()
 		if keyid is False:
 			return False
-		p = Popen([settings.GPG_BIN, '--list-key', keyid], stdout=PIPE, stderr=PIPE)
+		cmd = self.GPGCommand('--list-key', keyid)
+		p = Popen(cmd, stdout=PIPE, stderr=PIPE)
 		out, err = p.communicate()
 		if out != '':
 			return out.split("\n")
@@ -58,18 +74,25 @@ class GpgKey(models.Model):
 		'''
 		override djangos save method to add key to keyring
 		'''
-		tempkey = NamedTemporaryFile()
-		try:
-			tempkey.write(self.keydata)
-			p = Popen([settings.GPG_BIN, '--import', tempkey.name])
-			p.communicate() # wait for p to finish
-		finally:
-			tempkey.close()
-		super(Model, self).save(*args, **kwargs)
+		super(GpgKey, self).save(*args, **kwargs)
 
 	def sendKey(self):
 		'''send myself to a key server'''
 		return True
+
+	def GPGCommand(self, *args):
+		cmd = [settings.GPG_BIN]
+		cmd += self._getGPGAdditionalOptions()
+		cmd += list(args)
+		return cmd
+
+	def _getGPGAdditionalOptions(self):
+		opts = []
+		if hasattr(settings, 'GPG_KEYRING_FILE') and settings.GPG_KEYRING_FILE != '':
+			opts.append('--no-default-keyring')
+			opts.append('--keyring')
+			opts.append(settings.GPG_KEYRING_FILE)
+		return opts
 
 	def __unicode__(self):
 		return self.getKeyID()
@@ -80,21 +103,26 @@ class Mail(models.Model):
 	gpgkey = models.ManyToManyField(GpgKey) #make sure you sign this uid before adding
 
 	def sign(self, key):
+		if key not in self.gpgkey.all():
+			return False
+
 		# find uid number
 		uidlist = [] # list of uids without other key stuff
 		myuid = 0 # uids start at 1
-		for l in g.listKey():
-			if re.search(r"^uid", l):
+		for l in key.listKey():
+			if search(r"^uid", l):
 				uidlist.append(l)
-		for i,l in uidlist:
+		for i,l in enumerate(uidlist):
 			if str(self) in l:
 				myuid = i+1
 				break
 
 		# "gpg --default-cert-check-level 3 --edit-key 8E3DDB55C67FFA78 uid 3 sign save" and enter y
-		p = Popen([settings.GPG_BIN, '--default-cert-check-level', '3', '--edit-key', str(self.gpgkey), 'uid', str(myuid), 'sign', 'save'], stdout=PIPE, stderr=PIPE, stdin=PIPE)
-		out, err = p.communicate(input="y") #does anyone have a better idea?
-
+		cmd = key.GPGCommand('--yes', '--batch', '--default-cert-check-level', '3', '--edit-key', str(key), 'uid', str(myuid), 'sign', 'save')
+		p = Popen(cmd, stdout=PIPE, stderr=PIPE, stdin=PIPE)
+		#yes = Popen(['yes', 'y'], stdout=p.stdin)
+		out, err = p.communicate('yes')
+		#yes.wait()
 
 		# upload to key server
 		return True
